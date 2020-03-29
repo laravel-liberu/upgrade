c90a7ae40614bcbbfe97acdbaeebf08ac6c67248
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2020-03-29 17:37:04 +0300

    refactors native php function to collection for better readability

--- src/App/Helpers/Table.php
+++ src/App/Helpers/Table.php
@@ -2,14 +2,17 @@
 
 namespace LaravelEnso\Upgrade\App\Helpers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Schema;
 
 class Table
 {
     public static function hasIndex(string $table, string $index): bool
     {
-        return array_key_exists($index, Schema::getConnection()
-            ->getDoctrineSchemaManager()->listTableIndexes($table));
+        $currentIndexes = Schema::getConnection()->getDoctrineSchemaManager()
+            ->listTableIndexes($table);
+
+        return (new Collection($currentIndexes))->has($index);
     }
 
     public static function hasColumn(string $table, string $column): bool
